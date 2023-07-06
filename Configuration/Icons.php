9bdef58bd6452101abf0d1ba0c3ef3d027c07423
lenc99 <leon.encarnacao@iem.thm.de>
2023-07-06 20:35:28 +0200

    mask contnent-element and font imports

---
+++ Configuration/Icons.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    // Icon identifier
+    'graduationIcon' => [
+        'provider' => \FriendsOfTYPO3\FontawesomeProvider\Imaging\IconProvider\FontawesomeIconProvider::class,
+        // The FontAwesome icon name
+        'name' => 'graduation-cap',
+        // All icon providers provide the possibility to register an icon that spins
+        'spinning' => false,
+    ],
+    'calendarIcon' => [
+        'provider' => \FriendsOfTYPO3\FontawesomeProvider\Imaging\IconProvider\FontawesomeIconProvider::class,
+        // The FontAwesome icon name
+        'name' => 'calendar-days',
+        // All icon providers provide the possibility to register an icon that spins
+        'spinning' => false,
+    ],
+    'hourglassIcon' => [
+        'provider' => \FriendsOfTYPO3\FontawesomeProvider\Imaging\IconProvider\FontawesomeIconProvider::class,
+        // The FontAwesome icon name
+        'name' => 'hourglass',
+        // All icon providers provide the possibility to register an icon that spins
+        'spinning' => false,
+    ],
+];
